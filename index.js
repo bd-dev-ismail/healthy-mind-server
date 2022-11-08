067e09cd87a75420d1b361de43a5bb8453c36916
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-08 06:33:57 +0600

    connected with database

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 //midlewares
 app.use(cors());
 app.use(express());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nbna82s.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+    try{
+        const servicesCollection = client.db('healthyMind').collection('services');
+    }
+    finally{
+
+    }
+}
+run().catch(err=> console.log(err))
+
 
 app.get('/', (req, res)=>{
     res.send('Healthy Mind Server is running');
