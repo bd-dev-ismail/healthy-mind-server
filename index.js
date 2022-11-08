02d314f4d22e03602d595651f074f8b4b8bf0e14
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-08 17:32:30 +0600

    get data from database

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 //midlewares
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 
@@ -19,12 +20,25 @@ const client = new MongoClient(uri, {
 async function run(){
     try{
         const servicesCollection = client.db('healthyMind').collection('services');
+        //create services
+        app.post('/services', async(req, res)=> {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            res.send(result);
+        });
+        //get services
+        app.get('/services', async(req, res)=> {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
     }
     finally{
 
     }
 }
-run().catch(err=> console.log(err))
+run().catch(err=> console.log(err.message))
 
 
 app.get('/', (req, res)=>{
